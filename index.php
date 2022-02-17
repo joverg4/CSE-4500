c0489a666e54e83415d37167e844322a8865636d
joverg4 <92398674+joverg4@users.noreply.github.com>
2022-02-16 23:11:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -11,77 +11,116 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,400;0,800;1,400;1,800&family=Saira+Condensed:wght@500;700&family=Saira+Extra+Condensed:wght@500;600;700&display=swap" rel="stylesheet">
-    <link href="https://youli.academic.csusb.edu/css/style.css" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,400;0,800;1,400;1,800&family=Saira+Extra+Condensed:wght@500;600;700&display=swap" rel="stylesheet">
+    <link href="https://youli.academic.csusb.edu/css/style.css" rel="stylesheet" type="text/css" />
   </head>
-  <body data-bs-spy="scroll" data-bs-target="#navbar-example3" style="position:relative" >
+  <body data-bs-spy="scroll" data-bs-target="#navbar-example3" style="position:relative">
     <div class="left-nav">
-      <nav id="navbar-example3" class="navbar vertical-center flex-column center">
-        <nav class="nav flex-column">
-          <img src="avatar.png" style="width:160px" alt="">
+
+      <nav id="navbar-example3" class="navbar flex-column center vertical-center p-3">
+          <img src="avatar.png" style="width:160px" ></img>
           <a class="nav-link" href="#about">About</a>
           <a class="nav-link" href="#education">Education</a>
           <a class="nav-link" href="#experience">Experience</a>
-        </nav>
+          <a class="nav-link" href="#skills">Skills</a>
+          <a class="nav-link" href="#interests">Interests</a>
       </nav>
-    </div>
 
+    </div>
     <div data-bs-offset="0" tabindex="0">
+
       <div id="about" class="content-item">
         <div class="vertical-center">
-          <h1><?php echo $json_data['first-name']; ?><span class="text-primary"><?php echo $json_data['last-name']; ?></span></h1>
-          <div class="subheading mb-3">
-            <?php echo $json_data['address']; ?> · <?php echo $json_data['phone']; ?> ·
-            <a href="<?php echo $json_data['email']; ?>"><?php echo $json_data['email']; ?></a>
+          <h1>Firstname Lastname</h1>
+          <div class="subheading mb-5">
+            5500 University Pkwy, San Bernardino, CA 92407  .  (123)456-7890  .  name@email.com
           </div>
-          <p><?php echo $json_data['introduction']; ?></p>
+          <p>This is an introuduction of yourself.</p>
         </div>
       </div>
-    </div>
-
-      <hr>
 
       <div id="education" class="content-item">
         <div class="vertical-center">
-          <h2>Education</h2>
-          <?php foreach($json_data['education'] AS $education) { ?>
-              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="flex-grow-1">
-                  <h3 class="mb-0"><?php echo $education['school']; ?></h3>
-                  <div class="subheading mb-3"><?php echo $education['major']; ?></div>
-                  <p><?php echo $education['description']; ?></p>
-                </div>
-                <div class="flex-shrink-0">
-                  <span class="text-primary"><?php echo $education['period']; ?></span>
-                </div>
+          <h2 class="mb-5">Education</h2>
+
+          <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div class="flex-grow-1">
+              <h3>CSUSB</h3>
+              <div class="subheading">
+                Computer Science
               </div>
-          <?php  } ?>
+              <p>Some description about your academic performace.</p>
+            </div>
+            <div class="flex-shrink-0">
+              <span class="text-primary">March 2020 - Present</span>
+            </div>
+          </div>
+
         </div>
       </div>
-
-      <hr>
 
       <div id="experience" class="content-item">
         <div class="vertical-center">
-          <h4>Experience</h4>
-          <?php foreach($json_data['experience'] AS $experience) { ?>
-              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="flex-grow-1">
-                  <h5 class="mb-0"><?php echo $experience['title']; ?></h5>
-                  <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
-                  <p><?php echo $experience['description']; ?></p>
-                </div>
-                <div class="flex-shrink-0">
-                  <span class="text-primary"><?php echo $experience['period']; ?></span>
-                </div>
+          <h2 class="mb-5">Experience</h2>
+
+          <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div class="flex-grow-1">
+              <h3>Web Developer</h3>
+              <div class="subheading">
+                CSUSB
               </div>
-          <?php  } ?>
+              <p>Some description about your web developing experience.</p>
+            </div>
+            <div class="flex-shrink-0">
+              <span class="text-primary">March 2021 - Present</span>
+            </div>
+          </div>
+
+          <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div class="flex-grow-1">
+              <h3>Application Developer</h3>
+              <div class="subheading">
+                CSUSB
+              </div>
+              <p>Some description about your application developing experience.</p>
+            </div>
+            <div class="flex-shrink-0">
+              <span class="text-primary">March 2021 - Present</span>
+            </div>
+          </div>
+
         </div>
       </div>
-      <hr>
+      
+      <div id="skills" class="content-item">
+        <div class="vertical-center">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <ul>
+              <li>Programming languages: C++, php, python, Java, JavaScipts, HTML, CSS</li>
+              <li>System design</li>
+              <li>System Administration</li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+      
+      <div id="interests" class="content-item">
+        <div class="vertical-center">
+          <h2 class="mb-5">Interests</h2>
+
+          <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <p>Some descriptsion here ....</p>
+          </div>
+
+        </div>
+      </div>
 
     </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
